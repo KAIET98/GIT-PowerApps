7fdee8e7a64a5386ecf43f03c6aad1b644cef512
KAIET98 <KAIET98>
2023-08-14 13:52:57 +0200

    Se ha cambiado el codiog de sql para crear las tablas

--- sql_scripts/dimension_usuarios.sql
+++ sql_scripts/dimension_usuarios.sql
@@ -2,7 +2,7 @@
 
 
 CREATE TABLE dimension_usuarios (
-id_usuario identity(1,1) not null primary key, 
+id_usuario int identity(1,1) not null primary key, 
 nombre VARCHAR not null, 
 apellido_1 VARCHAR not null, 
 apellido_2 VARCHAR not null, 
@@ -16,7 +16,7 @@ fecha_alta VARCHAR not null,
 fecha_baja VARCHAR,
 id_idioma INT not null,
 id_alergia int null,
-FOREIGN KEY (id_idioma) REFERENCES dimension_idiomas(id_idioma), 
+FOREIGN KEY (id_idioma) REFERENCES dbo.dimension_idiomas(id_idioma), 
 FOREIGN KEY (id_alergia) REFERENCES dimension_alergias(id_alergia)
 
 
