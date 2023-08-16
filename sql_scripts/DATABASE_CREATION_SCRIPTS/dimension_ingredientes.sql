db075811a6e2821440f1161e98223d6a11134234
KAIET98 <KAIET98>
2023-08-16 13:51:29 +0200

    Modificando las queries de creacion de tablas y añadiendo alguna consulta primeriza

--- sql_scripts/DATABASE_CREATION_SCRIPTS/dimension_ingredientes.sql
+++ sql_scripts/DATABASE_CREATION_SCRIPTS/dimension_ingredientes.sql
@@ -3,7 +3,7 @@
 
 CREATE TABLE dimension_ingredientes(
 
-id_ingrediente int  identity(1,1) not null primary key,
+id_ingrediente int  not null primary key,
 id_idioma int not null,
 nombre_ingrediente varchar(4000) not null,
 FOREIGN KEY (id_idioma) REFERENCES dimension_idiomas(id_idioma)
